7dd9242dbc530e8113e7789a91fb7e166f7245ab
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-02-17 17:47:27 +0200

    Time: 8 ms (5.23%), Space: 43.6 MB (19.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        ArrayList<String> indeces = new ArrayList<String>();
+        HashMap<String,String> indeces = new HashMap<String,String>();
         fill(image,sr,sc,color,indeces);
         return image;
     }
-    public static void fill(int[][] image, int sr, int sc, int color, ArrayList<String> indeces){
-        if(indeces.contains(sr+" "+sc)) return;
+    public static void fill(int[][] image, int sr, int sc, int color, HashMap<String,String> indeces){
+        if(indeces.containsKey(sr+" "+sc)) return;
         int n = image.length;
         int m = image[0].length;
         int oldColor = image[sr][sc];
         image[sr][sc] = color;
-        indeces.add(sr+" "+sc);
+        indeces.put(sr+" "+sc,"");
         if(sr>0 && image[sr-1][sc] == oldColor) fill(image,sr-1,sc,color,indeces);
         if(sr<n-1 && image[sr+1][sc] == oldColor) fill(image,sr+1,sc,color,indeces);
         if(sc>0 && image[sr][sc-1] == oldColor) fill(image,sr,sc-1,color,indeces);
