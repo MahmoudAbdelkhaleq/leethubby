bd9c521fb6c40c9137e5a5a9cbf3bebad0225a6d
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-02-18 00:07:42 +0200

    Time: 34 ms (13.56%), Space: 43.1 MB (25.68%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        for(int i =0;i<ransomNote.length();i++){
+            if(magazine.contains(ransomNote.charAt(i)+""))
+                magazine = magazine.replaceFirst(ransomNote.charAt(i)+"","");
+            else return false;
+        }
+        return true;
+    }
+}
