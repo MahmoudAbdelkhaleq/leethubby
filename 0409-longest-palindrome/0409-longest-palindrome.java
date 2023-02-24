f2c68282ab6d5513572adb045c88cd16a1e08284
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-02-24 20:26:27 +0200

    Time: 15 ms (6.68%), Space: 41.1 MB (39.51%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> data = new HashMap<Character,Integer>();
+        for(int i =0;i<s.length();i++){
+            if(data.containsKey(s.charAt(i)))
+                data.put(s.charAt(i),data.get(s.charAt(i))+1);
+            else
+                data.put(s.charAt(i),1);
+        }
+        int max = 0;
+        boolean oneLetter = false;
+        for (int count : data.values()){
+            System.out.println(count);
+            if(count%2==0) max +=count;
+            else{
+                max+=count-1;
+                oneLetter = true;
+            }
+        }
+        if(oneLetter) max++;
+        return max;
+    }
+}
