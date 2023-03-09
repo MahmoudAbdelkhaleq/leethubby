67f4341ab13478d711e9a6fa6ef725676f6e67c7
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-03-09 13:40:37 +0200

    Time: 41 ms (40.36%), Space: 43.7 MB (20.10%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int sum=0;
+        int max=0;
+        for(int i =0;i<piles.length;i++){
+            sum+=piles[i];
+            if(piles[i]>max)max = piles[i];
+        }
+        int min =1;
+        while(min<max){
+            int mid = min+(max-min)/2;
+            if(verify(mid,piles,h)){
+                max=mid;
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        return min;
+    }
+    public static boolean verify(int num, int [] piles, int h){
+        int hours = 0;
+        for(int i =0;i<piles.length;i++){
+            hours = hours+(int)Math.ceil((piles[i]+0.0)/num);
+        }
+        if(hours<=h) return true;
+        return false;
+    }
+}
