f84a694b224f667f731a7fd43e71bd276567fbfc
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-07-21 22:44:55 +0300

    Time: 83 ms (38.91%), Space: 43.4 MB (57.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -8,9 +8,6 @@ class Solution {
             for (int j = 0; j < i; ++j)
                 if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
                     dp[i] = dp[j] + 1;
-        for(int i = 0;i<dp.length;i++){
-            System.out.print(dp[i] + " ");
-        }
         return Arrays.stream(dp).max().getAsInt();
     }
 }
