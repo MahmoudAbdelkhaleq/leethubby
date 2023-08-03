722b0b1eb2dfecc95e7e1da580826ef36f004a85
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-03 18:21:30 +0300

    Time: 9 ms (93.21%), Space: 44 MB (93.74%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,15 +1,28 @@
 class Solution {
     public int partitionString(String s) {
-        String current = "";
-        int count = 0;
-        for(int i = 0;i<s.length();i++){
-            if(current.contains(s.charAt(i)+"")){
-                current = "";
+        // String current = "";
+        // int count = 0;
+        // for(int i = 0;i<s.length();i++){
+        //     if(current.contains(s.charAt(i)+"")){
+        //         current = "";
+        //         count++;
+        //     }
+        //     current+=s.charAt(i)+"";
+        // }
+        // if(!current.equals(""))count++;
+        // return count;
+        int[] lastSeen = new int[26];
+        Arrays.fill(lastSeen, -1);
+        int count = 1, substringStart = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (lastSeen[s.charAt(i) - 'a'] >= substringStart) {
                 count++;
+                substringStart = i;
             }
-            current+=s.charAt(i)+"";
+            lastSeen[s.charAt(i) - 'a'] = i;
         }
-        if(!current.equals(""))count++;
+
         return count;
     }
 }
