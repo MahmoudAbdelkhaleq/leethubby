77878d6798c7388065ac0ed6d39f6e9b7d014af5
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-10 14:17:37 +0300

    Time: 0 ms (100.00%), Space: 43.1 MB (49.12%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -6,33 +6,5 @@ class Solution {
             }
         }
         return false;
-        // int n = nums.length;
-        // int start = 0;
-        // int end = n-1;
-        // while(start<end){
-        //     int mid = start+(end-start)/2;
-        //     if(nums[mid]==nums[n-1]){
-        //         end++;
-        //         start++;
-        //         continue;
-        //     }
-        //     if(nums[mid]>nums[n-1]){
-        //         start = mid+1;
-        //     }
-        //     else end = mid-1;
-        // }
-        // return  BS(nums, target, 0, start) ||
-        //         BS(nums, target, start+1, n-1);
-    }
-    private boolean BS(int[]nums, int target, int start, int end){
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(nums[mid]==target)
-                return true;
-            if(nums[mid]>target)
-                end = mid-1;
-            else start = mid+1;
-        }
-        return false;
     }
 }
