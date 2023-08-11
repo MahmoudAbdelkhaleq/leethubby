a81e4170f7549f6077446ef101eb244a063c6796
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-11 22:45:55 +0300

    Time: 17 ms (99.80%), Space: 54.9 MB (83.32%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,28 +1,17 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         Arrays.sort(nums);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i =0;i<nums.length;i++){
-            list.add(nums[i]);
-        }
         int ops =0;
         int i =0;
-        int j =list.size()-1;
+        int j =nums.length-1;
         while(i<j){
-            int x = list.get(i);
-            int y = list.get(j);
-            if(x+y==k){
-                list.remove(j);
-                list.remove(i);
-                ops++;
-                j = j-2;
-            }
-            if(x+y>k){
-                j--;
-            }
-            if(x+y<k){
+            if(nums[i]+nums[j]==k){
                 i++;
+                j--;
+                ops++;
             }
+            else if(nums[i]+nums[j]>k) j--;
+            else i++;
         }
         return ops;
     }
