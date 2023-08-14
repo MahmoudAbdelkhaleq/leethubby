5da3a56cf0b41ed257e37ce0b60708e007ee99f5
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-14 10:42:29 +0300

    Time: 18 ms (46.32%), Space: 42.8 MB (91.53%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        return pass(root, sum);
+    }
+    public int pass(TreeNode root, long sum) {
+        if (root == null) return 0;
+        return pathSumFrom(root, sum) + pass(root.left, sum) + pass(root.right, sum);
+    }
+    
+    private int pathSumFrom(TreeNode node, long sum) {
+        if (node == null) return 0;
+        return (node.val == sum ? 1 : 0) 
+            + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
+    }
+}
