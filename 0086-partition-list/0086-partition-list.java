a919343db565ab1c02b4fded5f6896ad67dd142a
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-15 20:14:48 +0300

    Time: 0 ms (100.00%), Space: 40.8 MB (83.63%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -12,27 +12,6 @@ class Solution {
     public ListNode partition(ListNode head, int x) {
         if(head == null) return null;
         if(head.next == null) return head;
-//         ListNode prevLess;
-//         if(head.val<x) prevLess = head;
-//         else prevLess = null;
-//         ListNode current = head.next;
-//         ListNode prev = head;
-//         while(current.next!=null){
-//             if(current.val>=x){
-//                 current = current.next;
-//                 prev = prev.next;
-//             }
-//             else{
-//                 if(prev == null){
-//                     ListNode tmp = current.next;
-//                     current.next = prevLess;
-//                     prev.next = tmp;
-                    
-//                     current = tmp;
-//                     head = 
-//                 }
-//             }
-//         }
         ListNode current = head;
         ListNode res = null;
         ListNode resCurrent = null;
