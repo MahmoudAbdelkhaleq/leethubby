6c2aa980561092c3fa3322f30b24ea5f4eecf744
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-21 16:29:09 +0300

    Time: 74 ms (54.81%), Space: 44 MB (74.78%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,18 +1,22 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int size = 1;
-        int plus = 1;
-        if(s.length()%2==1) plus = 2;
-        while(size<=s.length()/2){
-            int index = size;
-            String sample = s.substring(0,size);
-            while(index+size<=s.length()){
-                if(!sample.equals(s.substring(index, index+size))) break;
-                index+=size;
-            }
-            if(index==s.length()) return true;
-            size+=plus;
-        }
+        // int size = 1;
+        // int plus = 1;
+        // if(s.length()%2==1) plus = 2;
+        // while(size<=s.length()/2){
+        //     int index = size;
+        //     String sample = s.substring(0,size);
+        //     while(index+size<=s.length()){
+        //         if(!sample.equals(s.substring(index, index+size))) break;
+        //         index+=size;
+        //     }
+        //     if(index==s.length()) return true;
+        //     size+=plus;
+        // }
+        // return false;
+        String t = s + s;
+        if (t.substring(1, t.length() - 1).contains(s))
+            return true;
         return false;
     }
 }
