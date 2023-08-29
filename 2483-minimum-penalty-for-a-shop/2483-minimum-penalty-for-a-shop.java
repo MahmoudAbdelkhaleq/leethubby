ac1618fc786c08e71adf590e2fe76e83e0d5e179
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-08-29 14:13:00 +0300

    Time: 9 ms (91.25%), Space: 44.5 MB (34.01%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -2,9 +2,9 @@ class Solution {
     public int bestClosingTime(String customers) {
         int index = 0;
         int min = 0;
-        for(int i =0;i<customers.length();i++){
-            if(customers.charAt(i)=='Y')min++;
-        }
+        // for(int i =0;i<customers.length();i++){
+        //     if(customers.charAt(i)=='Y')min++;
+        // }
         int currentPenalty = min;
         for(int i =0;i<customers.length();i++){
             if(customers.charAt(i)=='Y')currentPenalty--;
