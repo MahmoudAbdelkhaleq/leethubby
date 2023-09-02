130ef29fd8ee6d0d21d7dce4fae17625b832dd33
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2023-09-02 12:31:39 +0300

    Time: 45 ms (50.86%), Space: 44.4 MB (26.00%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,17 +1,18 @@
 class Solution {
     public int minExtraChar(String s, String[] dictionary) {
         int [] dp = new int [s.length()+1];
+        Set<String> dictionarySet = new HashSet<>(Arrays.asList(dictionary));
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
         for(int i =1;i<=s.length();i++){
             String current = s.substring(0,i);
-            if(contains(dictionary, current)){
+            if(dictionarySet.contains(current)){
                 dp[i]=0;
             }
             else {
                 for (int j = current.length() - 1; j > -1; j--) {
                     String secondHalf = current.substring(j);
-                    if (contains(dictionary, secondHalf)) {
+                    if (dictionarySet.contains(secondHalf)) {
                         dp[i] = Math.min(dp[i], dp[j]);
                     } else {
                         dp[i] = Math.min(dp[i], dp[j] + secondHalf.length());
@@ -20,11 +21,5 @@ class Solution {
             }
         }
         return dp[s.length()];
-    }
-    private boolean contains(String [] arr, String s){
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i].equals(s)) return true;
-        }
-        return false;
     }
 }
