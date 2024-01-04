48b59f1a6b98ad559c0a913510dad5597c9796f5
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2024-01-04 15:10:15 +0200

    Time: 1 ms (100.00%), Space: 53.2 MB (48.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,45 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int max=0;
-        int index = -1;
-        boolean oneZero = false;
-        boolean twoZeros = false;
-        for(int i =0;i<nums.length;i++){
-            if(Math.abs(nums[i])>=max){
-                max = nums[i];
-                index = i;
-            }
-            if(nums[i]==0 && oneZero) twoZeros = true;
-            if(nums[i]==0) oneZero = true;
+        int n = nums.length;
+        int result[] = new int[n];
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
-        int productButMax = 1;
-        for(int i=0;i<nums.length;i++){
-            if(i!=index){
-                productButMax*=nums[i];
-            }
+        int suffixProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffixProduct;
+            suffixProduct *= nums[i];
         }
-        int[]result = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(i==index)result[i]=productButMax;
-            else{
-                if(nums[i]!=0){
-                    result[i]=(productButMax/nums[i])*max;
-                }
-                else{
-                    if(twoZeros) result[i]=0;
-                    else{
-                        result[i]=maxButZeros(nums);
-                    }
-                }
-            }
-        }
+
         return result;
-    }
-    public static int maxButZeros(int[] nums){
-        int x = 1;
-        for(int i =0;i<nums.length;i++){
-            if(nums[i]!=0) x*=nums[i];
-        }
-        return x;
     }
 }
