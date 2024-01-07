10262d7cbb621545aed618846e1e3d19ea676302
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2024-01-07 11:27:00 +0200

    Time: 53 ms (23.78%), Space: 43.8 MB (60.19%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,11 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int [] dp = new int [nums.length];
+        int [] dp = new int[nums.length];
         Arrays.fill(dp, 1);
-        for (int i = 0; i < nums.length; ++i)
-            for (int j = 0; j < i; ++j)
-                if (nums[i] > nums[j] && dp[i] < dp[j] + 1)
+        for(int i = 1; i<dp.length;i++){
+            for(int j = 0;j<i;j++){
+                if(nums[j]<nums[i] && dp[j] >= dp[i])
                     dp[i] = dp[j] + 1;
-        return Arrays.stream(dp).max().getAsInt();
+            }
+        }
+        int max = 1;
+        for(int i =1;i<dp.length;i++){
+            if(dp[i]>max) max = dp[i];
+        }
+        return max;
     }
+    
 }
