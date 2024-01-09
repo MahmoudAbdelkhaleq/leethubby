787ab685c035cd91ec3a0406f78f92c32b2d5418
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2024-01-10 01:13:09 +0200

    Time: 9 ms (89.59%), Space: 44.8 MB (79.35%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,14 +1,22 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
         int res = 0;
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
+        int first = 0;
         for(int i = 0;i<bank.length;i++){
-            int count = countOnes(bank[i]);
-            if(count != 0) list.add(count);
+            if(first == 0){
+                first = countOnes(bank[i]);
+                continue;
+            }
+            int second = countOnes(bank[i]);
+            if(second != 0){
+                res+=first*second;
+                first = second;
+            }
         }
-        for(int i = 0;i<list.size()-1;i++){
-            res+=list.get(i)*list.get(i+1);
-        }
+        // for(int i = 0;i<list.size()-1;i++){
+        //     res+=list.get(i)*list.get(i+1);
+        // }
         return res;
     }
     private int countOnes(String row){
