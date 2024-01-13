cc3525b0a65924ab1c17fbfd0fee3314f3ab89de
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2024-01-13 12:59:07 +0200

    Time: 6 ms (34.00%), Space: 45.7 MB (22.56%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -3,22 +3,16 @@ class Solution {
         if(nums.length == 2) return nums;
         int [] res= new int[2];
         int count = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0;i<nums.length && count<2;i++){
-            if(!set.contains(nums[i])){
-                boolean found = false;
-                for(int j = i+1;j<nums.length;j++){
-                    if(nums[i] == nums[j]){
-                        set.add(nums[i]);
-                        found = true;
-                        break;
-                    }
-                }
-                if(!found){
-                    res[count++] = nums[i];
-                }
-            }
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
         }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+               res[count++]=entry.getKey();
+            }
+            if(count == 2)break;
+        } 
         return res;
     }
 }
