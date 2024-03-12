03af2147d9daae38412cc7f7facc301423046821
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhalek5o5o@users.noreply.github.com>
2024-03-12 11:23:15 +0200

    Time: 3 ms (55.77%), Space: 43.4 MB (35.41%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,24 +1,27 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int i = 0;int j = 0;
-        Set<Integer> res = new HashSet<>();
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i] == nums2[j]){
-                res.add(nums2[j]);
-                i++;j++;
-            }
-            else{
-                if(nums1[i]>nums2[j]) j++;
-                else i++;
-            }
-        }
-        int [] resArr = new int [res.size()];
-        i=0;
-        for(int n:new ArrayList<>(res)){
-            resArr[i++] = n;
-        }
-        return resArr;
+    public int[] setIntersection(Set<Integer> set1, Set<Integer> set2) {
+    int[] output = new int[set1.size()];
+    int idx = 0;
+    for (Integer s : set1) {
+        if (set2.contains(s)){
+            output[idx++] = s;
+        } 
     }
+    return Arrays.copyOf(output, idx);
+  }
+  public int[] intersection(int[] nums1, int[] nums2) {
+    Set<Integer> set1 = new HashSet<>();
+    for (Integer n : nums1) {
+        set1.add(n);
+    } 
+    Set<Integer> set2 = new HashSet<>();
+    for (Integer n : nums2) {
+        set2.add(n);
+    }
+    if (set1.size() < set2.size()){
+        return setIntersection(set1, set2);
+    } else {
+        return setIntersection(set2, set1);
+    } 
+  }
 }
