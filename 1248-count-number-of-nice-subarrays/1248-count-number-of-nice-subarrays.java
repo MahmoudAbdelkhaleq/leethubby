0feea47adf7ea8d307749467e21603923ee8fe19
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-06-22 21:52:16 +0300

    Time: 6 ms (97.91%), Space: 54.9 MB (45.80%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
+    // public int numberOfSubarrays(int[] nums, int k) {
+    //     int count = 0;
+    //     int prefixOdds = 0;
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     map.put(0, 1);
+    //     for(int i = 0;i<nums.length;i++){
+    //         prefixOdds+=nums[i]%2;
+    //         if(map.containsKey(prefixOdds-k)){
+    //             count+=map.get(prefixOdds-k);
+    //         }
+    //         map.put(prefixOdds, map.getOrDefault(prefixOdds, 0) + 1);
+    //     }
+    //     return count;
+    // }
+    public int numberOfSubarrays(int [] nums, int k){
         int count = 0;
-        int prefixOdds = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
+        int start = 0;
+        int evenVals = 0;
+        int oddVals = 0;
         for(int i = 0;i<nums.length;i++){
-            prefixOdds+=nums[i]%2;
-            if(map.containsKey(prefixOdds-k)){
-                count+=map.get(prefixOdds-k);
+            oddVals+=nums[i]%2;
+            if(oddVals == k){
+                evenVals = 0;
+                while(oddVals == k){
+                    oddVals-=nums[start++]%2;
+                    evenVals++;
+                }
             }
-            map.put(prefixOdds, map.getOrDefault(prefixOdds, 0) + 1);
+            count+=evenVals;
         }
         return count;
     }
