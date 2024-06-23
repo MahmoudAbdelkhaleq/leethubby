2c8bee60bd572192c268bb543ee1e120ef5b8702
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-06-23 22:05:05 +0300

    Time: 56 ms (37.73%), Space: 57.3 MB (74.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,31 +1,39 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
         // num, {lowerlength, upperlength}
-        HashMap<Integer, Integer[]> map = new HashMap<>();
-        for(int num:nums){
-            map.put(num, new Integer[]{0, 0});
-        }
+        HashMap<Integer, Integer> map = new HashMap<>();
         int max = 0;
-        HashSet<Integer> covered = new HashSet<>();
-        for(Map.Entry<Integer, Integer[]> row: map.entrySet()){
-            int key = row.getKey();
-            Integer[] value = row.getValue();
-            if(!covered.contains(key)){
-                covered.add(key);
-                while(map.containsKey(key+value[1]+1)){
-                    value[1]++;
-                    covered.add(key+value[1]+1);
-                }
-                while(map.containsKey(key-value[0]-1)){
-                    value[0]++;
-                    covered.add(key-value[0]-1);
-                }
-            }
-            int length = value[0] + value[1] + 1;
-            if(max<length){
-                max = length;
+        for(int num:nums){
+            if(!map.containsKey(num)){
+                int leftLength = map.getOrDefault(num-1,0);
+                int rightLength = map.getOrDefault(num+1,0);
+                int length = leftLength + rightLength + 1;
+                max = Math.max(max, length);
+                map.put(num, length);
+                map.put(num - leftLength, length);
+                map.put(num + rightLength, length);
             }
         }
+        // HashSet<Integer> covered = new HashSet<>();
+        // for(Map.Entry<Integer, Integer[]> row: map.entrySet()){
+        //     int key = row.getKey();
+        //     Integer[] value = row.getValue();
+        //     if(!covered.contains(key)){
+        //         covered.add(key);
+        //         while(map.containsKey(key+value[1]+1)){
+        //             value[1]++;
+        //             covered.add(key+value[1]+1);
+        //         }
+        //         while(map.containsKey(key-value[0]-1)){
+        //             value[0]++;
+        //             covered.add(key-value[0]-1);
+        //         }
+        //     }
+        //     int length = value[0] + value[1] + 1;
+        //     if(max<length){
+        //         max = length;
+        //     }
+        // }
         return max;
     }
 }
