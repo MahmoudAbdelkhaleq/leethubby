1bf043928a8dc36d4121c3d38d6f72ba7bc67ac3
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-07-07 16:09:42 +0300

    Time: 101 ms (50.00%), Space: 162.3 MB (50.00%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -2,25 +2,7 @@ class Solution {
     public int numberOfSubmatrices(char[][] grid) {
         int [][][] dp = new int[grid.length][grid[0].length][2];
         int count = 0;
-        dp[0][0][0] = grid[0][0] == 'X'? 1:0;
-        dp[0][0][1] = grid[0][0] == 'Y'? 1:0;
-        // first row
-        for(int i = 1;i<grid[0].length;i++){
-            if(grid[0][i] == 'X'){
-                dp[0][i][0] = dp[0][i-1][0]+1;
-                dp[0][i][1] = dp[0][i-1][1];
-            }
-            else if(grid[0][i] == 'Y'){
-                dp[0][i][0] = dp[0][i-1][0];
-                dp[0][i][1] = dp[0][i-1][1]+1;
-            }
-            else{
-                dp[0][i][0] = dp[0][i-1][0];
-                dp[0][i][1] = dp[0][i-1][1];
-            }
-            if(dp[0][i][0] == dp[0][i][1] && dp[0][i][0]!=0) count++;
-        }
-        for(int i = 1;i<grid.length;i++){
+        for(int i = 0;i<grid.length;i++){
             for(int j = 0;j<grid[0].length;j++){
                 if(j==0){
                     if(grid[i][0] == 'X'){
@@ -49,6 +31,12 @@ class Solution {
                 for(int j = 0;j<dp[0].length;j++){
                     dp[i][j][0]+=dp[i-1][j][0];
                     dp[i][j][1]+=dp[i-1][j][1];
+                    if(dp[i][j][0] == dp[i][j][1] && dp[i][j][0]!=0)
+                        count++;
+                }
+            }
+            else{
+                for(int j = 0;j<dp[0].length;j++){
                     if(dp[i][j][0] == dp[i][j][1] && dp[i][j][0]!=0)
                         count++;
                 }
