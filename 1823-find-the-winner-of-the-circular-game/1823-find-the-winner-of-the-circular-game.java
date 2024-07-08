1b5b63cda9b10cafaf1636a2ca9ba2fdf09525c2
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-07-08 08:07:16 +0300

    Time: 0 ms (100.00%), Space: 40.6 MB (50.17%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,14 +1,38 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        List<Integer> nums = new ArrayList<>();
-        for(int i = 1;i<=n;i++){
-            nums.add(i);
+        return winnerHelper(n, k) + 1;
+        // List<Integer> nums = new ArrayList<>();
+        // for(int i = 1;i<=n;i++){
+        //     nums.add(i);
+        // }
+        // int index = 0;
+        // for(int i = 0;i<n-1;i++){
+        //     index = (index + k - 1)%nums.size();
+        //     nums.remove(index);
+        // }
+        // return nums.get(0);
+        
+        // Queue<Integer> queue = new LinkedList<>();
+        // for(int i = 1;i<=n;i++){
+        //     queue.offer(i);
+        // }
+        // int count = 1;
+        // while(queue.size()>1){
+        //     if(count == k){
+        //         queue.poll();
+        //         count = 1;
+        //     }
+        //     else{
+        //         queue.offer(queue.poll());
+        //         count++;
+        //     }
+        // }
+        // return queue.poll();
+    }
+    private int winnerHelper(int n, int k) {
+        if (n == 1) {
+            return 0;
         }
-        int index = 0;
-        for(int i = 0;i<n-1;i++){
-            index = (index + k - 1)%nums.size();
-            nums.remove(index);
-        }
-        return nums.get(0);
+        return (winnerHelper(n - 1, k) + k) % n;
     }
 }
