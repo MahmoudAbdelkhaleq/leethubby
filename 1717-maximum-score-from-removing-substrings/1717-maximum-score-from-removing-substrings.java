883d80552d913998e50fda20becc5bca921a8c2c
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-07-17 13:13:42 +0300

    Time: 1867 ms (5.00%), Space: 55.3 MB (5.01%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -1,40 +1,34 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
         String higher = x>y? "ab":"ba";
+        String lower = higher.equals("ba")? "ab":"ba";
         int higherScore = x>y?x:y;
         int lowerScore = x>y?y:x;
         int score = 0;
         Stack<Character> stack = new Stack<>();
+        score+=removePair(s, higher, higherScore, stack);
+        StringBuilder remaining = new StringBuilder();
+        while(!stack.isEmpty()){
+            remaining.insert(0,stack.pop());
+        }
+        s = remaining.toString();
+        score+=removePair(s, lower, lowerScore, stack);
+        return score;
+    }
+    private int removePair(String s, String pair, int pairScore, Stack<Character> stack){
+        int score = 0;
         for(char c:s.toCharArray()){
             if(stack.isEmpty()){
                 stack.push(c);
             }
             else{
-                if(higher.equals(stack.peek()+""+c)){
+                if(pair.equals(stack.peek()+""+c)){
                     stack.pop();
-                    score+=higherScore;
+                    score+=pairScore;
                 }
                 else{
                     stack.push(c);
                 }
-            }
-        }
-        Stack <Character> stack2 = new Stack<>();
-        if(!stack.isEmpty()){
-            stack2.push(stack.pop());
-        }
-        while(!stack.isEmpty()){
-            char c1 = stack.pop();
-            char c2 = stack2.pop();
-            if(higher.equals(c2+""+c1)){
-                score+=lowerScore;
-                if(!stack.isEmpty() && stack2.isEmpty()){
-                    stack2.push(stack.pop());
-                }
-            }
-            else{
-                stack2.push(c2);
-                stack2.push(c1);
             }
         }
         return score;
