3ab437654ba3528a19071b1b48c32ddaad42bac8
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-08-19 09:04:26 +0300

    Time: 5 ms (64.07%), Space: 40.3 MB (65.41%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -8,9 +8,9 @@ class Solution {
         return dp[n];
     }
     public int gcd(int n){
-        for (int i = n / 2; i >= 1; i--) {
+        for (int i = 2; i <= n/2; i++) {
             if (n % i == 0) {
-                return i;
+                return n/i;
             }
         }
         return 1;
