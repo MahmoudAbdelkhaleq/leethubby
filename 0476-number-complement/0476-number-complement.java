96d1d2b784897e92ff7bd6307dc5fff1d2b18128
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-08-22 07:58:25 +0300

    Time: 2 ms (12.59%), Space: 40.7 MB (12.78%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findComplement(int num) {
+        int bits = getLog(num);
+        long roof = (long) Math.pow(2, bits) - 1;
+        System.out.println(roof);
+        return (int)(roof-num);
+    }
+    public int getLog(int num){
+        int log = 0;
+        for(long i = 1;i<=num;i*=2){
+            log++;
+        }
+        return log;
+    }
+}
