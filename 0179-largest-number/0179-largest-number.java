d6c0031cdc9870dd1b0b3f26b37093ded70a99fc
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-09-18 09:00:52 +0300

    Time: 3 ms (99.36%), Space: 41.6 MB (99.79%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String [] numWords = new String[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            numWords[i] = nums[i]+"";
+        }
+        Arrays.sort(numWords, new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2){
+                int i = 0;
+                while(i<s1.length() && i<s2.length()){
+                    if(s1.charAt(i)>s2.charAt(i)){
+                        return -1;
+                    }
+                    if(s2.charAt(i)>s1.charAt(i)){
+                        return 1;
+                    }
+                    i++;
+                }
+                if(s1.length()>s2.length()){
+                    int res = s2.charAt(0)-s1.charAt(i);
+                    if(res == 0){
+                        return compare(s1.substring(i),s2);
+                    }
+                    return res;
+                }
+                if(s2.length()>s1.length()){
+                    int res = s2.charAt(i)-s1.charAt(0);;
+                    if(res == 0){
+                        return compare(s1,s2.substring(i));
+                    }
+                    return res;
+                }
+                return 0;
+            }
+        });
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        for(;i<numWords.length-1;i++){
+            if(!numWords[i].equals("0")){
+                break;
+            }
+        }
+        for(;i<numWords.length;i++){
+            res.append(numWords[i]);
+        }
+        return res.toString();
+    }
+}
