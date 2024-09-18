a5e364fa79bc65ba258002ce919dbaeccac50dfe
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-09-18 10:45:55 +0300

    Time: 3 ms (99.36%), Space: 41.7 MB (99.36%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -8,33 +8,22 @@ class Solution {
             @Override
             public int compare(String s1, String s2){
                 int i = 0;
-                while(i<s1.length() && i<s2.length()){
-                    if(s1.charAt(i)>s2.charAt(i)){
-                        return -1;
-                    }
-                    if(s2.charAt(i)>s1.charAt(i)){
-                        return 1;
-                    }
-                    i++;
+                for(;i<s1.length() && i<s2.length();i++){
+                    int res = s2.charAt(i) - s1.charAt(i);
+                    if(res != 0) return res;
                 }
                 if(s1.length()>s2.length()){
-                    int res = s2.charAt(0)-s1.charAt(i);
-                    return res != 0 ? res : compare(s1.substring(i),s2);
+                    return compare(s1.substring(i),s2);
                 }
                 if(s2.length()>s1.length()){
-                    int res = s2.charAt(i)-s1.charAt(0);;
-                    return res != 0 ? res : compare(s1,s2.substring(i));
+                    return compare(s1,s2.substring(i));
                 }
                 return 0;
             }
         });
         StringBuilder res = new StringBuilder();
         int i = 0;
-        for(;i<numWords.length-1;i++){
-            if(!numWords[i].equals("0")){
-                break;
-            }
-        }
+        for(;i<numWords.length-1 && numWords[i].equals("0");i++){}
         for(;i<numWords.length;i++){
             res.append(numWords[i]);
         }
