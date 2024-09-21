36af5bd37e20378a12bb19e8433f0d42645fb3fd
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-09-21 09:10:12 +0300

    Time: 2 ms (72.07%), Space: 47.6 MB (95.89%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,25 +1,16 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        int digits = 0;
-        int k = n;
-        while(k>0){
-            digits++;
-            k = k/10;
-        }
         List<Integer> res = new ArrayList<>();
         for(int i = 1;i<10;i++)
-            recursion(res, ""+i, digits, n);
+            recursion(res, i, n);
         return res;
     }
-    void recursion(List<Integer> res, String num, int digits, int n){
-        int number = Integer.parseInt(num);
-        if(num.length()<digits){
+    void recursion(List<Integer> res, int number, int n){
+        if(number<=n){
             res.add(number);
-            for(int i = 0;i<10;i++)
-                recursion(res, num+i,digits,n);
-        }
-        else if(Integer.parseInt(num) <= n){
-            res.add(number);
+            for(int i = 0;i<10 && number <= n/10;i++){
+                recursion(res, number*10+i,n);
+            }
         }
     }
 }
