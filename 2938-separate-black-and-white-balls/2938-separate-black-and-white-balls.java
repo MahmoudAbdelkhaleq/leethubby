b6b80e5cace641bfa3749e0a085c0ec4316f23be
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-10-15 09:24:03 +0300

    Time: 7 ms (100.00%), Space: 45.2 MB (70.97%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -1,24 +1,33 @@
 class Solution {
     public long minimumSteps(String s) {
         long steps = 0;
-        int blackBalls = 0;
-        for(char c:s.toCharArray()){
-            if(c == '1')
-                blackBalls++;
-        }
-        List<Integer> rightWhitePositions = new ArrayList<Integer>();
-        for(int i = s.length()-1;i>s.length()-1-blackBalls;i--){
-            char c = s.charAt(i);
-            if(c == '0')
-                rightWhitePositions.add(i);
-        }
-        for(int i = 0;i<s.length()-blackBalls;i++){
-            char c = s.charAt(i);
-            if(c == '1'){
-                steps+=rightWhitePositions.get(0)-i;
-                rightWhitePositions.remove(0);
+        int blackBallCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                steps += blackBallCount;
+            } else {
+                blackBallCount++;
             }
         }
+
         return steps;
+        // for(char c:s.toCharArray()){
+        //     if(c == '1')
+        //         blackBalls++;
+        // }
+        // List<Integer> rightWhitePositions = new ArrayList<Integer>();
+        // for(int i = s.length()-1;i>s.length()-1-blackBalls;i--){
+        //     char c = s.charAt(i);
+        //     if(c == '0')
+        //         rightWhitePositions.add(i);
+        // }
+        // for(int i = 0;i<s.length()-blackBalls;i++){
+        //     char c = s.charAt(i);
+        //     if(c == '1'){
+        //         steps+=rightWhitePositions.get(0)-i;
+        //         rightWhitePositions.remove(0);
+        //     }
+        // }
+        // return steps;
     }
 }
