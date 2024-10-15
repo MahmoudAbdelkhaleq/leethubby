16b8574d1aa46f0620f4ee20696169965f8f0eb2
Mahmoud Abdelkhaleq <65434738+MahmoudAbdelkhaleq@users.noreply.github.com>
2024-10-15 09:21:50 +0300

    Time: 263 ms (5.02%), Space: 45.9 MB (5.02%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long steps = 0;
+        int blackBalls = 0;
+        for(char c:s.toCharArray()){
+            if(c == '1')
+                blackBalls++;
+        }
+        List<Integer> rightWhitePositions = new ArrayList<Integer>();
+        for(int i = s.length()-1;i>s.length()-1-blackBalls;i--){
+            char c = s.charAt(i);
+            if(c == '0')
+                rightWhitePositions.add(i);
+        }
+        for(int i = 0;i<s.length()-blackBalls;i++){
+            char c = s.charAt(i);
+            if(c == '1'){
+                steps+=rightWhitePositions.get(0)-i;
+                rightWhitePositions.remove(0);
+            }
+        }
+        return steps;
+    }
+}
